37d65e7ead5503ef3ca6d8c3a6bc52ed29013509
Jessica Wong <wong.jessica0411@gmail.com>
2019-08-06 14:40:37 -0400

    added logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <header className="App-header">
         <p>
           placeholder dot co of isun
