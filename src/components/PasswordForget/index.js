34da997ab1cf55d7954ad8c204f8fcc3fc4ea237
Jessica Wong <wong.jessica0411@gmail.com>
2019-08-07 16:02:00 -0400

    successfully implemented authentication

--- src/components/PasswordForget/index.js
+++ src/components/PasswordForget/index.js
@@ -1,9 +1,86 @@
-import React from 'react';
+/*
+The password forget uses a form to submit the information (email address) needed by the Firebase authentication API to reset the password.
+A class method (onSubmit) ensures the information is sent to the API.
+It also resets the form’s input field on a successful request, and shows an error on an erroneous request.
+The form is validated before it is submitted as well.
+The file implements a password forget link as a component which isn’t used directly in the form component.
+*/
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-const PasswordForget = () => (
+import { withFirebase } from '../Firebase';
+import * as ROUTES from '../../constants/routes';
+
+const PasswordForgetPage = () => (
   <div>
     <h1>PasswordForget</h1>
+    <PasswordForgetForm />
   </div>
 );
 
-export default PasswordForget;
+const INITIAL_STATE = {
+  email: '',
+  error: null,
+};
+
+class PasswordForgetFormBase extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { ...INITIAL_STATE };
+  }
+
+  onSubmit = event => {
+    const { email } = this.state;
+
+    this.props.firebase
+      .doPasswordReset(email)
+      .then(() => {
+        this.setState({ ...INITIAL_STATE });
+      })
+      .catch(error => {
+        this.setState({ error });
+      });
+
+    event.preventDefault();
+  };
+
+  onChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  render() {
+    const { email, error } = this.state;
+
+    const isInvalid = email === '';
+
+    return (
+      <form onSubmit={this.onSubmit}>
+        <input
+          name="email"
+          value={this.state.email}
+          onChange={this.onChange}
+          type="text"
+          placeholder="Email Address"
+        />
+        <button disabled={isInvalid} type="submit">
+          Reset My Password
+        </button>
+
+        {error && <p>{error.message}</p>}
+      </form>
+    );
+  }
+}
+
+const PasswordForgetLink = () => (
+  <p>
+    <Link to={ROUTES.PASSWORD_FORGET}>Forgot Password?</Link>
+  </p>
+);
+
+export default PasswordForgetPage;
+
+const PasswordForgetForm = withFirebase(PasswordForgetFormBase);
+
+export { PasswordForgetForm, PasswordForgetLink };
